eb01f1894e99cb1e16f44c4716d89e2d1e6d377f
KritikaAdhikari <107241786+KritikaAdhikari@users.noreply.github.com>
2024-11-23 18:37:18 +0545

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 5 ; int n2 = 4;
+            Console.WriteLine($"Before Swapping = {n1},{n2}");
+            n1 = n1 * n2;
+            n2 = n1 / n2;
+            n1 = n1 / n2;
+            Console.WriteLine($"After Swapping = {n1},{n2}");
+        }
+    }
+}
